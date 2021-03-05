2ab1edd87a4964722860f8764f2b2892a9b6a367
Kevin <kevinator900@gmail.com>
2021-03-05 15:23:32 -0700

    worked on variety of stuff

--- script.js
+++ script.js
@@ -37,4 +37,44 @@ $(function(){
         
         previousScroll = currentScroll;
      });
+
+    //  $('.color-ball').offset({top: 283})
+
+    //  $('.color-ball').on("mousedown", dragMouseDown)
+    //  let distance = 0, oldMouseY = 0;
+    //  function dragMouseDown(event){
+    //     event = event || window.event;
+    //     event.preventDefault()
+
+    //     oldMouseY = event.clientY;
+    //     $(this).on("mouseup", closeDrag)
+    //     $(this).on("mousemove", elementDrag)
+    //  }
+
+    //  function elementDrag(event){
+    //     event = event || window.event
+    //     event.preventDefault()
+    //     offset = $(this).offset().top
+    //     //283 - 425
+    //     if(offset <= 425 && offset >= 283){
+    //         distance = oldMouseY - event.clientY;
+    //         oldMouseY = event.clientY;
+    //         $(this).offset({top: offset - distance})
+    //     }
+    //     if(offset > 425){
+    //         $(this).offset({top: 420})
+    //         console.log(`fixed`)
+    //     }else if(offset < 283){
+    //         $(this).offset({top: 290})
+    //         console.log(`fixed`)
+    //     }
+
+    //     let color = `rgb(255, ${425 - offset}, 100)`
+    //     document.documentElement.style.setProperty("--bg-color", color)
+    //  }
+    //  function closeDrag() {
+    //     $(this).off("mousemove", elementDrag);
+    //   }
+
+
 })
