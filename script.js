65220bd88f01cf7b61ad616b7f9d71482915885f
Kevin <kevinator900@gmail.com>
2021-03-08 15:27:49 -0700

    everything broke because I wanted a fancy transition

--- script.js
+++ script.js
@@ -38,43 +38,40 @@ $(function(){
         previousScroll = currentScroll;
      });
 
-    //  $('.color-ball').offset({top: 283})
+     $('.skills-container').hide();
+     $('.resume-container').hide();
+     $('.contact-container').hide();
 
-    //  $('.color-ball').on("mousedown", dragMouseDown)
-    //  let distance = 0, oldMouseY = 0;
-    //  function dragMouseDown(event){
-    //     event = event || window.event;
-    //     event.preventDefault()
+     function changeSection(newSection){
+        let $activeSection = $('.active-container')[0];
+        $('.active-container').addClass('oldContainer')
+        let $newSection = $(`#${newSection}`);
+        //Does it need to change?
+        if($activeSection == $newSection){
+            return false;
+        }
+        //  Remove Old Section
+        $activeSection.animate({
+            left: '-200rem'
+        }, 1000)
+        setTimeout(resetContainer, 1000)
 
-    //     oldMouseY = event.clientY;
-    //     $(this).on("mouseup", closeDrag)
-    //     $(this).on("mousemove", elementDrag)
-    //  }
-
-    //  function elementDrag(event){
-    //     event = event || window.event
-    //     event.preventDefault()
-    //     offset = $(this).offset().top
-    //     //283 - 425
-    //     if(offset <= 425 && offset >= 283){
-    //         distance = oldMouseY - event.clientY;
-    //         oldMouseY = event.clientY;
-    //         $(this).offset({top: offset - distance})
-    //     }
-    //     if(offset > 425){
-    //         $(this).offset({top: 420})
-    //         console.log(`fixed`)
-    //     }else if(offset < 283){
-    //         $(this).offset({top: 290})
-    //         console.log(`fixed`)
-    //     }
-
-    //     let color = `rgb(255, ${425 - offset}, 100)`
-    //     document.documentElement.style.setProperty("--bg-color", color)
-    //  }
-    //  function closeDrag() {
-    //     $(this).off("mousemove", elementDrag);
-    //   }
+        //  Show New Section
+        
 
 
+     }
+
+     function resetContainer(){
+        let oldContainer = $('.oldContainer')
+        oldContainer.removeClass('active-section')
+        oldContainer.hide()
+        oldContainer.css("left", "0")
+        oldContainer.removeClass('oldContainer')
+     }
+
+     $("#contact-link").on("click", function(){changeSection('contact')})
+     $("#landing-link").on("click", function(){changeSection('landing')})
+     $("#resume-link").on("click", function(){changeSection('resume')})
+     $("#skills-link").on("click", function(){changeSection('skills')})
 })
