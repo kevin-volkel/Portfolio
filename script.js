7474d1c2dc827252299133a6736593f72e1411e2
Kevin <kevinator900@gmail.com>
2021-03-09 15:27:20 -0700

    commiting for gitpages

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
 $(function(){
-    // $('.right-card').css("left",1500)
-    // $('.left-card').css("right",1500)
-    // let currCard = 1;
-    // let scrollTop = 400;
+         //Animating the Skill Cards 
 
-    // $(window).scroll(function(){
-    //     if($(this).scrollTop() > scrollTop){
-    //         $(`.right-card${currCard}`).animate({
-    //             left: 0
-    //         }, 250)
-    //         $(`.left-card${currCard}`).animate({
-    //             right: 0
-    //         }, 300)
-    //         currCard++;
-    //         scrollTop += 300;
-    //     }
-    // })
+   //  $('.right-card').css("left",1500)
+   //  $('.left-card').css("right",1500)
+   //  let currCard = 1;
+   //  let scrollTop = 400;
 
-    // let previousScroll = 0;
-    // let animating = false;
-    // $(window).scroll(function(){
-    //     var currentScroll = $(this).scrollTop();
-    //     if(!animating){
-    //         animating = true;
-    //         if (currentScroll > previousScroll){
-    //             console.log(`down`)
-    //             $('nav').slideUp(500)
-    //         } else {
-    //             console.log(`up`)
-    //             $('nav').slideDown(500)
-    //         }
-    //         setTimeout(function(){
-    //             animating = false;
-    //         }, 500)
-    //     }
+   //  $(window).scroll(function(){
+   //      if($(this).scrollTop() > scrollTop){
+   //          $(`.right-card${currCard}`).animate({
+   //              left: 0
+   //          }, 250)
+   //          $(`.left-card${currCard}`).animate({
+   //              right: 0
+   //          }, 300)
+   //          currCard++;
+   //          scrollTop += 300;
+   //      }
+   //  })
+
+         // Animating the Navbar
+
+   //  let previousScroll = 0;
+   //  let animating = false;
+   //  $(window).scroll(function(){
+   //      var currentScroll = $(this).scrollTop();
+   //      if(!animating){
+   //          animating = true;
+   //          if (currentScroll > previousScroll){
+   //              console.log(`down`)
+   //              $('nav').slideUp(500)
+   //          } else {
+   //              console.log(`up`)
+   //              $('nav').slideDown(500)
+   //          }
+   //          setTimeout(function(){
+   //              animating = false;
+   //          }, 500)
+   //      }
         
-    //     previousScroll = currentScroll;
-    //  });
+   //      previousScroll = currentScroll;
+   //   });
 
      $('#skills').hide();
      $('#resume').hide();
